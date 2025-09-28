0cf978e8ed19f4850b646841aa43f7966e22a0a8
thegreatfeez <adamsafeez4@gmail.com>
2025-09-28 10:17:51 +0100

    setp: setup components && pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Layout from "./components/Layout"
 import Decks from "./Pages/Decks"
 import Stats from "./Pages/Stats"
+import Home from "./Pages/Home"
+import Login from "./Pages/Login"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+         <Route index element={<Home />} />
           <Route path="decks" element={<Decks />} />
           <Route path="stats" element={<Stats />} />
         </Route>
+         <Route path="login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
   );
